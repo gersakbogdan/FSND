673a0b2d392a64f1366f7d4fbd8763025d2b85e2
Bogdan Gersak <bogdan@gersak.ro>
2015-07-25 15:33:26 +0000

    Update tree.py

--- PythonFoundation/Turtle/tree.py
+++ PythonFoundation/Turtle/tree.py
@@ -1,25 +1,24 @@
 import random
 import turtle
 
-def tree(branchLenght, t):
-    t.pensize(branchLenght / 10)
+def tree(size, myTurtle):
+    myTurtle.pensize(size / 10)
 
-    if branchLenght < random.randint(1,2) * 20:
-        t.color("green")
+    if size < random.randint(1,2) * 20:
+        myTurtle.color("green")
     else:
-        t.color("brown")
+        myTurtle.color("brown")
 
-    if branchLenght > 5:
-        t.forward(branchLenght)
-        t.left(25)
-        tree(branchLenght - random.randint(10, 20),t)
-        t.right(50)
-        tree(branchLenght - random.randint(10, 20),t)
-        t.left(25)
-        color = t.pencolor()
-        t.penup()
-        t.backward(branchLenght)
-        t.pendown()
+    if size > 5:
+        myTurtle.forward(size)
+        myTurtle.left(25)
+        tree(size - random.randint(10, 20),t)
+        myTurtle.right(50)
+        tree(size - random.randint(10, 20),t)
+        myTurtle.left(25)
+        myTurtle.penup()
+        myTurtle.backward(size)
+        myTurtle.pendown()
 
 
 window = turtle.Screen()
